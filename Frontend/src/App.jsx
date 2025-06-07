2a249d1bca979304e6eadac1d0727e306e6d0031
RealVineetCoder <kumarvineet4893@gmail.com>
2025-06-07 20:37:56 +0530

    resolved merge conflicts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import Signin from './pages/Signin';
-import BusinessProfile from './pages/Profile'; 
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import Signin from "./pages/Signin";
+import Home from "./pages/home";
+import Layout from "@/layout"
+import BusinessProfile from "./pages/profile";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signin" element={<Signin />} />
         <Route path="/profile" element={<BusinessProfile />} />
+        
+        <Route
+          path="/"
+          element={<Layout><Home /></Layout>}
+        />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
---
+++ Frontend/src/pages/home.jsx
@@ -0,0 +1,47 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items
+const items = [
+  { title: "Home", url: "#", icon: Home },
+  { title: "Inbox", url: "#", icon: Inbox },
+  { title: "Calendar", url: "#", icon: Calendar },
+  { title: "Search", url: "#", icon: Search },
+  { title: "Settings", url: "#", icon: Settings },
+];
+
+export default function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton>
+                    <a href={item.url} className="flex items-center gap-2 ">
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
